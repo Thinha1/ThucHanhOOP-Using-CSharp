1c363ad5a8597e3861e92f866b77412a7edb792f
Thinha1 <Thinhanhdang014@gmail.com>
2025-03-25 14:00:08 +0700

    Create Program.cs

---
+++ Tuan03/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2001230930_DangAnhThinh_Buoi02_Bai2
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+            DSNhanVien ds1 = new DSNhanVien();
+            String f = "E:\\LuuDuLieuSinhVien\\DangAnhThinh2001230930\\2001230930_DangAnhThinh_Buoi02_Bai2\\2001230930_DangAnhThinh_Buoi02_Bai2\\DSNhanVien.xml";
+            ds1.docFile(f);
+            ds1.xuatDSNV();
+        }
+    }
+}
